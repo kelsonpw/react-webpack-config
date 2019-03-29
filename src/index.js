47895dcf6a013b7177c0d795f99d66836d92637a
Kelson Warner <kelson@null.net>
2019-03-29 10:09:37 -0700

    Update to functional component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./styles.scss";
 
-class App extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
+function App(){
+  return (
+    <div>Hello there.  Such simple app.</div>
+  )
 }
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+render(<App />, document.getElementById("app"));
